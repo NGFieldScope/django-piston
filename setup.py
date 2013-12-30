81e92678a10a1cc73522d4225918f36f8550b7f6
Eric Russell <erussell@ngs.org>
2013-12-30 11:45:12 -0500

    More updates for Django 1.5 compatability -- replace simplejson with stdlib json

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import os
 
 setup(
     name = "django-piston",
-    version = "0.2.5-mc-eor",
+    version = "0.2.6-mc-eor",
     url = 'http://bitbucket.org/mattcaldwell/django-piston/wiki/Home',
 	download_url = 'http://bitbucket.org/mattcaldwell/django-piston/downloads/',
     license = 'BSD',
